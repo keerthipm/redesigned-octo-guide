c26e483cb54b9ce5aabe8416e419e6d4122e6373
keerthipm <111999360+keerthipm@users.noreply.github.com>
2022-08-24 12:12:13 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/*
+Hello.java
+Keerthi Malempati
+08/24/2022
+CMSC255 004
+*/
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Keerthi Malempati");
+		System.out.println("Hello.java");
+		System.out.println("August 24th, 2022");
+		System.out.println("This program will run and print out my name, program name, date, program description, course number, and section");
+		System.out.println("CMSC255 004");
+		System.out.println();
+	}
+}
